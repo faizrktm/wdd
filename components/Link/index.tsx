66ed7b41f121e65f2b3a47d4ef06dd9e68aad0aa
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-05-27 09:55:41 +0700

    feat: target blank

--- components/Link/index.tsx
+++ components/Link/index.tsx
@@ -1,6 +1,6 @@
-import type { ReactNode } from "react";
+import type { AnchorHTMLAttributes, ReactNode } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   location: string;
   text: string;
   type?: 'primary' | 'default' | 'primary_revert';
@@ -14,9 +14,9 @@ const BUTTON_TYPE = {
   primary_revert: 'text-primary bg-white',
 }
 
-export default function Link({ location, text, type = 'default', className, icon }: ButtonProps){
+export default function Link({ location, text, type = 'default', className, icon, ...rest }: ButtonProps){
   return (
-    <a href={location} className={`inline-flex items-center justify-center text-sm md:text-base rounded py-2 px-6 tracking-wider ${BUTTON_TYPE[type]} ${className || ''}`}>
+    <a href={location} className={`inline-flex items-center justify-center text-sm md:text-base rounded py-2 px-6 tracking-wider ${BUTTON_TYPE[type]} ${className || ''}`} {...rest}>
       {icon}{text}
     </a>
   )
