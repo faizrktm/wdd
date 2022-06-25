4a57bfc2b3c9b12272daeb0dad3bb39b7c15caed
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-06-25 15:57:34 +0700

    feat: various feature

---
+++ components/Gallery/Images/images.ts
@@ -0,0 +1,17 @@
+export const images = [
+  {
+    src: '/images/prewed/Noisess123.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/prewed/Noisess126.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/images/prewed/Noisess163.jpg',
+    width: 3,
+    height: 5,
+  }
+];
--- components/Gallery/Images/index.tsx
+++ components/Gallery/Images/index.tsx
@@ -2,19 +2,15 @@ import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import useShowModal from '@/components/Modal/hooks/useShowModal';
-
-const images = [
-  '/images/prewed/Noisess123.jpg',
-  '/images/prewed/Noisess126.jpg',
-  '/images/prewed/Noisess163.jpg',
-]
+import Gallery from 'react-photo-gallery';
+import { images } from './images';
 
 function ImagesModal({ selectedItem }: { selectedItem: number }){
   return (
     <Carousel showArrows={false} className="w-full h-[60vh]" showIndicators showStatus={false} infiniteLoop selectedItem={selectedItem || 0}>
       {images.map((item, index) => (
         <div key={index} style={{ width: '100%', height: '60vh' }}>
-          <Image src={item} layout="fill" objectFit="cover" />
+          <Image src={item.src} layout="fill" objectFit="cover" />
         </div>
       ))}
     </Carousel>
@@ -29,10 +25,10 @@ export default function Images() {
   };
 
   return (
-    <Carousel showArrows={false} className="w-full h-[400px]" showIndicators={false} showStatus={false} autoPlay infiniteLoop>
+    <Carousel showArrows={false} className="w-full h-[400px] md:w-[50%]" showIndicators={false} showStatus={false} autoPlay infiniteLoop>
       {images.map((item, index) => (
-        <button type="button" key={item} style={{ width: '100%', height: '400px' }} onClick={() => showModalSimpler(index)}>
-          <Image src={item} layout="fill" objectFit='cover' />
+        <button type="button" key={item.src} style={{ width: '100%', height: '400px' }} onClick={() => showModalSimpler(index)}>
+          <Image src={item.src} layout="fill" objectFit='cover' />
         </button>
       ))}
     </Carousel>
