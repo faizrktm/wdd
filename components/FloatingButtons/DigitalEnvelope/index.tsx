22c2f5baf1ead193d10363c876560a15a29663dd
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-07-03 14:43:29 +0700

    chore: revision

--- components/FloatingButtons/DigitalEnvelope/index.tsx
+++ components/FloatingButtons/DigitalEnvelope/index.tsx
@@ -7,7 +7,7 @@ const ModalEnvelope = dynamic(() => import('./ModalEnvelope'));
 
 function CtaEnvelope() {
   const showModal = useShowModal();
-  return <CircleButton type="primary" className="w-[60px] h-[60px]" text={<img alt="digital envelope" src="/images/donate.svg" width={36} height={36} />} onClick={() => showModal(<ModalEnvelope />, 'Amplop Digital')} />
+  return <CircleButton type="primary" className="w-[60px] h-[60px]" text={<img alt="digital envelope" src="/images/love-letter.svg" width={36} height={36} />} onClick={() => showModal(<ModalEnvelope />, 'Amplop Digital')} />
 }
 
 export default function DigitalEnvelope() {
