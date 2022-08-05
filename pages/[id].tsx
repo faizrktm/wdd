3552b28b6a5df9d516f8baeeeadc4deb36d156a7
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-08-05 21:25:33 +0700

    chore: subpath

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next';
 import {useState} from 'react';
 
-import Intro from '../components/Intro';
+import Intro from '@/components/Intro';
+import Hero from '@/components/Hero';
+import Couple from '@/components/Couple';
+import SaveDate from '@/components/SaveDate';
+import Location from '@/components/Location';
+import ThankYou from '@/components/ThankYou';
+import Gallery from '@/components/Gallery';
+import HealthProtocol from '@/components/HealthProtocol';
+import Wish from '@/components/Wish';
+import FloatingButtons from '@/components/FloatingButtons';
 
-const Home: NextPage = () => {
+const Invitation: NextPage = () => {
   const [open, setOpen] = useState(false);
 
   if(!open){
@@ -12,7 +21,19 @@ const Home: NextPage = () => {
     )
   }
 
-  return null;
+  return (
+    <>
+      <Hero />
+      <Couple />
+      <SaveDate />
+      <Location />
+      <ThankYou />
+      <Gallery />
+      <HealthProtocol />
+      <Wish />
+      <FloatingButtons />
+    </>
+  );
 }
 
-export default Home;
+export default Invitation;
