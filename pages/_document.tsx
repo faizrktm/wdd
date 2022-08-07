42cebc862f5cc2f518792d4f72bd2d7082cb35cf
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-08-07 13:10:06 +0700

    feat: update meta image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,12 +12,14 @@ class MyDocument extends Document {
           <meta name='description' content={description} />
           <meta property="og:title" content={title} />
           <meta property="og:type" content="website" />
-          <meta property="og:image" content="/images/meta.png" />
+          <meta property="og:image" content="/images/meta-new.png" />
 
           <meta name="twitter:title" content={title} />
           <meta name="twitter:description" content={description} />
-          <meta name="twitter:image" content="/images/meta.png" />
+          <meta name="twitter:image" content="/images/meta-new.png" />
           <meta name="twitter:card" content="summary_large_image" />
+
+          <link rel="shortcut icon" href="/favicon.ico" />
 
           <link href="https://fonts.googleapis.com/css2?family=Amita:wght@400&family=Cormorant:wght@400;600&family=Poppins:wght@200;400&display=swap" rel="stylesheet" />
         </Head>
