3fb50ddd9d6d5acbf3d4e11af2dca280f3c1db0b
Faiz Azmi Rekatama <faizkort@gmail.com>
2022-08-07 14:02:34 +0700

    feat: lite version (#6)
    * fix: og title
    
    * feat: serve lite version
    
    * feat: update meta image
    
    * chore: add favicon
    
    Co-authored-by: Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>

---
+++ pages/lite.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next';
+import {useState} from 'react';
+
+import Intro from '@/components/Intro';
+import Hero from '@/components/Hero';
+import Couple from '@/components/Couple';
+import SaveDate from '@/components/SaveDate';
+import Location from '@/components/Location';
+import ThankYou from '@/components/ThankYou';
+import Gallery from '@/components/Gallery';
+import HealthProtocol from '@/components/HealthProtocol';
+import Wish from '@/components/Wish';
+import FloatingButtons from '@/components/FloatingButtons';
+
+const Home: NextPage<{ guest?: string }> = ({ guest }) => {
+  const [open, setOpen] = useState(false);
+
+  if(!open) {
+    return (
+      <Intro onChange={setOpen} />
+    )
+  }
+
+  return (
+    <>
+      <Hero />
+      <Couple />
+      <SaveDate />
+      <Location />
+      <ThankYou />
+      <HealthProtocol />
+      <Wish guest={guest} />
+      <FloatingButtons />
+    </>
+  );
+}
+
+export default Home;
+
+export async function getServerSideProps(context) {
+  const guest = context?.query?.['lovely_guest'];
+  return {
+    props: {
+      guest: guest || '',
+    },
+  }
+}
