612ac26dfd6e5335eddb38b0458f9d1890d28b93
Faiz Azmi Rekatama <faizkort@gmail.com>
2022-08-11 22:07:54 +0700

    chore: static url

--- components/Meta/index.tsx
+++ components/Meta/index.tsx
@@ -7,15 +7,15 @@ export default function Meta({ guest }: { guest?: string }) {
   return (
     <Head>
       <title>{title}</title>
-      <meta name='description' content={description} />
+      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/images/meta-new.png" />
+      <meta property="og:image" content="https://www.faiz-rara-for.life/images/meta-new.png" />
 
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content="/images/meta-new.png" />
+      <meta name="twitter:image" content="https://www.faiz-rara-for.life/images/meta-new.png" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
