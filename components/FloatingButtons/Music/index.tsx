79bcdf29beb0c48fe0245bf1eeeba1535e2a31fd
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-08-14 19:29:26 +0700

    chore: fix auto play

--- components/FloatingButtons/Music/index.tsx
+++ components/FloatingButtons/Music/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CircleButton from "@/components/Button/Circle";
 
 export default function DigitalEnvelope() {
@@ -21,10 +21,24 @@ export default function DigitalEnvelope() {
     }
   }
 
+  useEffect(() => {
+    const play = () => {
+      player.current.play();
+    }
+    if(player.current){
+      player.current.addEventListener('canplay', play);
+    }
+    return () => {
+      if(player.current){
+        player.current.removeEventListener('canplay', play);
+      }
+    }
+  }, []);
+
   return (
     <>
       <CircleButton className={`w-[50px] h-[50px] ${paused ? 'bg-wind' : 'bg-blackish'}`} text={<img alt="digital envelope" src="/images/quaver.svg" width={26} height={26} />} onClick={() => stop()} />
-      <audio autoPlay ref={player} className="hidden">
+      <audio ref={player} className="hidden" preload="auto">
         <source src="/images/music.ogg" type="audio/ogg" />
         <source src="/images/music.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
