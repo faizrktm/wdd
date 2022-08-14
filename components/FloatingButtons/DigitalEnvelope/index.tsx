9439d610646bf5857edfa952d8d919c500ce6581
Faiz Azmi Rekatama <faiz.rekatama@tokopedia.com>
2022-08-14 19:50:55 +0700

    feat: emoji hearth

--- components/FloatingButtons/DigitalEnvelope/index.tsx
+++ components/FloatingButtons/DigitalEnvelope/index.tsx
@@ -30,7 +30,7 @@ function DigitalEnvelope() {
     <ModalProvider>
       <div className="relative flex items-center">
         <div id="tooltip-left" role="tooltip" className={`flex relative transition-opacity ease-in-out duration-1000 items-center max-w-[230px] z-10 py-2 px-3 mr-3 text-xs text-white bg-primary rounded-lg shadow-lg ${styleOpacity[showTooltip]} tooltip`}>
-          <p>Sekarang <b>amplopmu</b> bisa tersampaikan secara digital 😄</p>
+          <p>Sekarang <b>amplopmu</b> bisa tersampaikan secara digital &#10084;&#65039;</p>
           <button onClick={() => setShowTooltip(0)} type="button" className="absolute top-0 right-0 text-white bg-transparent hover:bg-primary hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </button>
